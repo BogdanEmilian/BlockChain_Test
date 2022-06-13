2d48aefa3c32a66a6d23c15048fac208b42c471a
BogdanEmilian <emilian.bogdan02@e-uvt.ro>
2022-06-13 23:25:56 +0300

    hashing algorithm for text data

--- src/main/java/files4blockchain/Application.java
+++ src/main/java/files4blockchain/Application.java
@@ -1,5 +1,8 @@
 package main.java.files4blockchain;
 
 public class Application {
-    
+    public static void main(String[] args)
+    {
+        System.out.println(SHA256Controller.hash("Text example"));
+    }
 }
--- src/main/java/files4blockchain/SHA256Controller.java
+++ src/main/java/files4blockchain/SHA256Controller.java
@@ -1,5 +1,29 @@
 package main.java.files4blockchain;
 
+import java.security.MessageDigest;
+
 public class SHA256Controller {
     
+    public static String hash(String data) {
+        try
+        {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            byte[] hash = digest.digest(data.getBytes("UTF-8"));
+
+            StringBuffer hexString = new StringBuffer();
+
+            for(int i=0; i<hash.length; i++)
+            {
+                String hexadecimal = Integer.toHexString(0xff & hash[i]);
+                hexString.append(hexadecimal);
+            }
+
+            return hexString.toString();
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+    }
 }
