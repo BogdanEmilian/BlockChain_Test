209522b7e68d68e2c089bf2fb01a00dcf5ff2ee7
BogdanEmilian <emilian.bogdan02@e-uvt.ro>
2022-06-16 16:29:53 +0300

    constants class parameters

--- src/main/java/files4blockchain/Constants.java
+++ src/main/java/files4blockchain/Constants.java
@@ -2,4 +2,12 @@ package main.java.files4blockchain;
 
 public class Constants {
     
+    private Constants() 
+    {
+        
+    }
+
+    public static final Integer difficulty = 1;
+    public static final Integer reward = 5;
+    public static final String genesis_hash = "0000000000000000000000000000000000000000000000000000000000000000";
 }
